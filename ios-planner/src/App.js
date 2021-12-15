a2449885fe46776a2567327a038125c497d00009
Anhelina4 <bogdanova_angelina@ukr.net>
2021-12-15 22:25:14 +0200

    fixed applayout, pagelayout

--- ios-planner/src/App.js
+++ ios-planner/src/App.js
@@ -1,13 +1,25 @@
 import Navigator from "./pages/Navigator"
 import "./styles/style.css"
-import { AppLayout } from "./components"
+import { AppLayout, Header, PageLayout, Sider } from "./components"
 import { PlannerProvider } from "./contexts"
+import { TasksAll } from "./pages/TasksAll"
 // Провайдер->Навігатор->Компонента
 
 function App() {
   return (
     <PlannerProvider>
-      <AppLayout></AppLayout>
+      <AppLayout
+        left={<Sider />}
+        right={
+          <PageLayout
+            header={
+              <Header color="blue" className="mb-xl">
+                New List
+              </Header>
+            }
+            content={<TasksAll />}
+          />
+        }></AppLayout>
     </PlannerProvider>
   )
 }
--- ios-planner/src/components/AppLayout/AppLayout.js
+++ ios-planner/src/components/AppLayout/AppLayout.js
@@ -1,27 +1,12 @@
 import React from "react"
 import { Container, Row, Col } from "../Grid"
-import { Sider } from ".."
-import { PageLayout } from ".."
-import { TaskSimpleForm, TaskList } from "../../domains/Task/components"
-import { Header } from "../../components"
-import {TasksAll} from "../../pages/TasksAll"
-const AppLayout = () => {
+
+const AppLayout = ({ left, right }) => {
   return (
     <Container height="100%" variant="fluid">
       <Row height="100%">
-        <Col cw="auto">
-          <Sider></Sider>
-        </Col>
-        <Col>
-          <PageLayout
-            header={
-              <Header color="blue" className="mb-xl">
-                New List
-              </Header>
-            }
-            content={<TasksAll/>}
-          />
-        </Col>
+        <Col cw="auto">{left}</Col>
+        <Col>{right}</Col>
       </Row>
     </Container>
   )
